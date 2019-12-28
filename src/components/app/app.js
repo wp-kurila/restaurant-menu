61e60587e04f6b04d2900149e44b2090c15cd7f8
wp-kurila <alendar94@mail.ru>
2019-12-28 23:36:22 +0300

    step 1

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,15 +1,27 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom';
 import {MainPage, CartPage} from '../pages';
 import AppHeader from '../app-header';
+import Error from '../error';
 
 import Background from './food-bg.jpg';
 
 const App = () => {
     return (
         <div style={{background: `url(${Background}) center center/cover no-repeat`}} className="app">
-            <AppHeader total={50}/>
-            <MainPage/>
-            <CartPage/>
+            <AppHeader total={50} />
+            <Switch>
+                <Route 
+                    exact 
+                    path='/'
+                    component={MainPage} />                
+                <Route 
+                    path='/cart'
+                    component={CartPage} /> 
+                <Route 
+                    path='*'
+                    component={Error} /> 
+            </Switch>
         </div>
     )
 }
